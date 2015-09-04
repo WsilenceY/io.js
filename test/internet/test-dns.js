acab305dab59d6e5278a97d13aaec588fc7ab016
Rich Trott <rtrott@gmail.com>
2015-09-03 23:17:34 -0700

    less abuse

--- test/internet/test-dns.js
+++ test/internet/test-dns.js
@@ -737,5 +737,4 @@ process.on('exit', function() {
   assert.equal(running, false);
   assert.strictEqual(expected, completed);
   assert.ok(getaddrinfoCallbackCalled);
-  assert.fail('just','making sure we fail', 'hi mom and dad', '=');
 });
