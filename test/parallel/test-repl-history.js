d921aa247558a3388aa642c8ec5d0bc92e1f6a35
Rich Trott <rtrott@gmail.com>
2015-10-16 00:30:25 -0700

    fix for 3392 but hopefully XeCycle solves it a better way!

---
+++ test/parallel/test-repl-history.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const common = require('../common');
+const assert = require('assert');
+const path = require('path');
+const fs = require('fs');
+const repl = require('internal/repl');
+
+// Invoking the REPL should create a repl history file at the specified path
+// and mode 600.
+
+common.refreshTmpDir();
+const replHistoryPath = path.join(common.tmpDir, 'repl_history');
+
+const checkResults = common.mustCall(function(err, r) {
+  if (err)
+    throw err;
+  r.input.end();
+  const stat = fs.statSync(replHistoryPath);
+  const mode = '0' + (stat.mode & parseInt('777', 8)).toString(8);
+  assert.strictEqual(mode, '0600', 'REPL history file should be mode 0600');
+});
+
+repl.createInternalRepl(
+  {NODE_REPL_HISTORY: replHistoryPath},
+  {terminal: true},
+  checkResults
+);
