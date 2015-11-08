b3a773f3e3e2b4e5b5e5255273ece36cbca2d0f4
Rich Trott <rtrott@gmail.com>
2015-11-08 12:45:05 -0800

    fixup: condense if statement per cjihrig

--- tools/eslint-rules/new-with-error.js
+++ tools/eslint-rules/new-with-error.js
@@ -11,17 +11,13 @@
 module.exports = function(context) {
 
   return {
-
     'ThrowStatement': function(node) {
-      if (node.argument.type === 'CallExpression') {
-        if (node.argument.callee.name === 'Error') {
-          context.report(node, 'Use new keyword when throwing.');
-        }
+      const arg = node.argument;
+      if ((arg.type === 'CallExpression') && (arg.callee.name === 'Error')) {
+        context.report(node, 'Use new keyword when throwing.');
       }
     }
-
   };
-
 };
 
 module.exports.schema = [];
