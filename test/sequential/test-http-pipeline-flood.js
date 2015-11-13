0b3c8735cd06266a62556dcbc1fe4481e42b9800
Rich Trott <rtrott@gmail.com>
2015-11-12 21:20:49 -0800

    master

--- test/sequential/test-http-pipeline-flood.js
+++ test/sequential/test-http-pipeline-flood.js
@@ -1,16 +1,6 @@
 'use strict';
-const common = require('../common');
-const assert = require('assert');
-
-// Here we are testing the HTTP server module's flood prevention mechanism.
-// When writeable.write returns false (ie the underlying send() indicated the
-// native buffer is full), the HTTP server cork()s the readable part of the
-// stream. This means that new requests will not be read (however request which
-// have already been read, but are awaiting processing will still be
-// processed).
-
-// Normally when the writable stream emits a 'drain' event, the server then
-// uncorks the readable stream, although we arent testing that part here.
+var common = require('../common');
+var assert = require('assert');
 
 switch (process.argv[2]) {
   case undefined:
@@ -18,49 +8,38 @@ switch (process.argv[2]) {
   case 'child':
     return child();
   default:
-    throw Error(`Unexpected value: ${process.argv[2]}`);
+    throw new Error('wtf');
 }
 
 function parent() {
-  const http = require('http');
-  const bigResponse = new Buffer(10240).fill('x');
+  var http = require('http');
+  var bigResponse = new Buffer(10240).fill('x');
   var gotTimeout = false;
   var childClosed = false;
   var requests = 0;
   var connections = 0;
-  var backloggedReqs = 0;
 
-  const server = http.createServer(function(req, res) {
+  var server = http.createServer(function(req, res) {
     requests++;
     res.setHeader('content-length', bigResponse.length);
-    if (!res.write(bigResponse)) {
-      if (backloggedReqs == 0) {
-        // Once the native buffer fills (ie write() returns false), the flood
-        // prevention should kick in.
-        // This means the stream should emit no more 'data' events. However we
-        // may still be asked to process more requests if they were read before
-        // mechanism activated.
-        // setImmediate(function () {
-        req.socket.on('data', () => common.fail('Unexpected data received'));
-        // });
-      }
-      backloggedReqs++;
-    }
-    res.end();
+    res.end(bigResponse);
   });
 
   server.on('connection', function(conn) {
     connections++;
   });
 
+  // kill the connection after a bit, verifying that the
+  // flood of requests was eventually halted.
   server.setTimeout(200, function(conn) {
     gotTimeout = true;
+    conn.destroy();
   });
 
   server.listen(common.PORT, function() {
-    const spawn = require('child_process').spawn;
-    const args = [__filename, 'child'];
-    const child = spawn(process.execPath, args, { stdio: 'inherit' });
+    var spawn = require('child_process').spawn;
+    var args = [__filename, 'child'];
+    var child = spawn(process.execPath, args, { stdio: 'inherit' });
     child.on('close', function(code) {
       assert(!code);
       childClosed = true;
@@ -72,26 +51,40 @@ function parent() {
     assert(gotTimeout);
     assert(childClosed);
     assert.equal(connections, 1);
+    // The number of requests we end up processing before the outgoing
+    // connection backs up and requires a drain is implementation-dependent.
+    // We can safely assume is more than 250.
+    console.log('server got %d requests', requests);
+    assert(requests >= 250);
+    console.log('ok');
   });
 }
 
 function child() {
-  const net = require('net');
+  var net = require('net');
 
-  const conn = net.connect({ port: common.PORT });
+  var gotEpipe = false;
+  var conn = net.connect({ port: common.PORT });
 
-  var req =
-    `GET / HTTP/1.1\r\nHost: localhost:${common.PORT}\r\nAccept: */*\r\n\r\n`;
+  var req = 'GET / HTTP/1.1\r\nHost: localhost:' +
+            common.PORT + '\r\nAccept: */*\r\n\r\n';
 
   req = new Array(10241).join(req);
 
   conn.on('connect', function() {
-    //kill child after 1s of flooding
-    setTimeout(function() { conn.destroy(); }, 1000);
     write();
   });
 
   conn.on('drain', write);
+
+  conn.on('error', function(er) {
+    gotEpipe = true;
+  });
+
+  process.on('exit', function() {
+    assert(gotEpipe);
+    console.log('ok - child');
+  });
 
   function write() {
     while (false !== conn.write(req, 'ascii'));
