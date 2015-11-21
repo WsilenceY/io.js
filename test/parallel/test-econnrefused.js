83e7a3845fbe1aa310105720b1f6d17c436000c3
Rich Trott <rtrott@gmail.com>
2015-11-21 15:45:45 -0800

    only fail the first time

--- test/parallel/test-econnrefused.js
+++ test/parallel/test-econnrefused.js
@@ -2,5 +2,14 @@
 
 const common = require('../common');
 const net = require('net');
+const fs = require('fs');
+const path = require('path');
 
-net.createConnection({port: common.PORT});
+const tmpFile = path.join(common.tmpDir, 'foo');
+
+try {
+  fs.accessSync(tmpFile);
+} catch (e) {
+  fs.openSync(tmpFile, 'w');
+  net.createConnection({port: common.PORT});
+}
