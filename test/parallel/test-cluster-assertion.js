a324dd06f4a558e5d52f33244d6385d4525058b4
Rich Trott <rtrott@gmail.com>
2015-12-08 14:03:27 -0800

    fixup

--- test/parallel/test-cluster-assertion.js
+++ test/parallel/test-cluster-assertion.js
@@ -1,10 +1,10 @@
-// In Node 5.1.1 on Linux, this test triggers an assertion in
-// internal/child_process.js.
+// In Node 5.1.1, this test throws an AssertionError in
+// `internal/child_process.js`.
 
 'use strict';
 const common = require('../common');
-//const assert = require('assert');
-//const net = require('net');
+const assert = require('assert');
+const net = require('net');
 const cluster = require('cluster');
 cluster.schedulingPolicy = cluster.SCHED_NONE;
 
@@ -14,11 +14,32 @@ if (cluster.isMaster) {
   worker1 = cluster.fork();
   worker1.on('message', common.mustCall(function() {
     worker2 = cluster.fork();
-    worker1.disconnect();
-    worker2.disconnect();
+    conn = net.connect(common.PORT, common.mustCall(function() {
+      worker1.disconnect();
+      worker2.disconnect();
+    }));
+    conn.on('error', function(e) {
+      // ECONNRESET is OK
+      if (e.code !== 'ECONNRESET')
+        throw e;
+    });
   }));
+
+  cluster.on('exit', function(worker, exitCode, signalCode) {
+    assert(worker === worker1 || worker === worker2);
+    assert.strictEqual(exitCode, 0);
+    assert.strictEqual(signalCode, null);
+    if (Object.keys(cluster.workers).length === 0)
+      conn.destroy();
+  });
 
   return;
 }
 
-process.send('go');
+var server = net.createServer(function(c) {
+  c.end('bye');
+});
+
+server.listen(common.PORT, function() {
+  process.send('listening');
+});
