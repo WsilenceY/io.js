4f8b5d70d36f2a7a8e9789b463c61dd19b3eec06
Rich Trott <rtrott@gmail.com>
2015-12-08 14:07:22 -0800

    rm conn

--- test/parallel/test-cluster-assertion.js
+++ test/parallel/test-cluster-assertion.js
@@ -9,28 +9,19 @@ const cluster = require('cluster');
 cluster.schedulingPolicy = cluster.SCHED_NONE;
 
 if (cluster.isMaster) {
-  var conn, worker1, worker2;
+  var worker1, worker2;
 
   worker1 = cluster.fork();
   worker1.on('message', common.mustCall(function() {
     worker2 = cluster.fork();
-    conn = net.connect(common.PORT, common.mustCall(function() {
-      worker1.disconnect();
-      worker2.disconnect();
-    }));
-    conn.on('error', function(e) {
-      // ECONNRESET is OK
-      if (e.code !== 'ECONNRESET')
-        throw e;
-    });
+    worker1.disconnect();
+    worker2.disconnect();
   }));
 
   cluster.on('exit', function(worker, exitCode, signalCode) {
     assert(worker === worker1 || worker === worker2);
     assert.strictEqual(exitCode, 0);
     assert.strictEqual(signalCode, null);
-    if (Object.keys(cluster.workers).length === 0)
-      conn.destroy();
   });
 
   return;
