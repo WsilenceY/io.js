f547ca6f437446529d9e12ee3e5f3e425181ad38
Rich Trott <rtrott@gmail.com>
2016-05-14 23:02:18 -0700

    test: add logging for test-debug-port-cluster
    The test is currently flaky and CI provides no real information because
    the test times out rather than failing on an assertion. Add logging to
    gather more information about the failure.
    
    Refs: https://github.com/nodejs/node/issues/6754

--- test/parallel/test-debug-port-cluster.js
+++ test/parallel/test-debug-port-cluster.js
@@ -1,12 +1,12 @@
 'use strict';
-var common = require('../common');
-var assert = require('assert');
-var spawn = require('child_process').spawn;
+const common = require('../common');
+const assert = require('assert');
+const spawn = require('child_process').spawn;
 
 const PORT_MIN = common.PORT + 1337;
 const PORT_MAX = PORT_MIN + 2;
 
-var args = [
+const args = [
   '--debug=' + PORT_MIN,
   common.fixturesDir + '/clustered-server/app.js'
 ];
@@ -16,12 +16,12 @@ child.stderr.setEncoding('utf8');
 
 let stderr = '';
 child.stderr.on('data', (data) => {
+  process.stderr.write(`[DATA] ${data}`);
   stderr += data;
-  if (child.killed !== true && stderr.includes('all workers are running'))
+  if (child.killed !== true && stderr.includes('all workers are running')) {
     child.kill();
-});
-
-process.on('exit', () => {
-  for (let port = PORT_MIN; port <= PORT_MAX; port += 1)
-    assert(stderr.includes(`Debugger listening on port ${port}`));
+    for (let port = PORT_MIN; port <= PORT_MAX; port += 1) {
+      assert(stderr.includes(`Debugger listening on port ${port}`));
+    }
+  }
 });
