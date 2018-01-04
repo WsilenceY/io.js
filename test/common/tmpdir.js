68492fa5e10bbe66b91033e3cf330de317376772
Rich Trott <rtrott@gmail.com>
2018-01-03 18:54:16 -0800

    wip: reduce everything exported by tmpdir except what's needed for exports.path

--- test/common/tmpdir.js
+++ test/common/tmpdir.js
@@ -1,54 +1,54 @@
-/* eslint-disable required-modules */
-'use strict';
+// /* eslint-disable required-modules */
+// 'use strict';
 
-const fs = require('fs');
+// const fs = require('fs');
 const path = require('path');
 
-function rimrafSync(p) {
-  let st;
-  try {
-    st = fs.lstatSync(p);
-  } catch (e) {
-    if (e.code === 'ENOENT')
-      return;
-  }
+// function rimrafSync(p) {
+//   let st;
+//   try {
+//     st = fs.lstatSync(p);
+//   } catch (e) {
+//     if (e.code === 'ENOENT')
+//       return;
+//   }
 
-  try {
-    if (st && st.isDirectory())
-      rmdirSync(p, null);
-    else
-      fs.unlinkSync(p);
-  } catch (e) {
-    if (e.code === 'ENOENT')
-      return;
-    if (e.code === 'EPERM')
-      return rmdirSync(p, e);
-    if (e.code !== 'EISDIR')
-      throw e;
-    rmdirSync(p, e);
-  }
-}
+//   try {
+//     if (st && st.isDirectory())
+//       rmdirSync(p, null);
+//     else
+//       fs.unlinkSync(p);
+//   } catch (e) {
+//     if (e.code === 'ENOENT')
+//       return;
+//     if (e.code === 'EPERM')
+//       return rmdirSync(p, e);
+//     if (e.code !== 'EISDIR')
+//       throw e;
+//     rmdirSync(p, e);
+//   }
+// }
 
-function rmdirSync(p, originalEr) {
-  try {
-    fs.rmdirSync(p);
-  } catch (e) {
-    if (e.code === 'ENOTDIR')
-      throw originalEr;
-    if (e.code === 'ENOTEMPTY' || e.code === 'EEXIST' || e.code === 'EPERM') {
-      const enc = process.platform === 'linux' ? 'buffer' : 'utf8';
-      fs.readdirSync(p, enc).forEach((f) => {
-        if (f instanceof Buffer) {
-          const buf = Buffer.concat([Buffer.from(p), Buffer.from(path.sep), f]);
-          rimrafSync(buf);
-        } else {
-          rimrafSync(path.join(p, f));
-        }
-      });
-      fs.rmdirSync(p);
-    }
-  }
-}
+// function rmdirSync(p, originalEr) {
+//   try {
+//     fs.rmdirSync(p);
+//   } catch (e) {
+//     if (e.code === 'ENOTDIR')
+//       throw originalEr;
+//     if (e.code === 'ENOTEMPTY' || e.code === 'EEXIST' || e.code === 'EPERM') {
+//       const enc = process.platform === 'linux' ? 'buffer' : 'utf8';
+//       fs.readdirSync(p, enc).forEach((f) => {
+//         if (f instanceof Buffer) {
+//           const buf = Buffer.concat([Buffer.from(p), Buffer.from(path.sep), f]);
+//           rimrafSync(buf);
+//         } else {
+//           rimrafSync(path.join(p, f));
+//         }
+//       });
+//       fs.rmdirSync(p);
+//     }
+//   }
+// }
 
 const testRoot = process.env.NODE_TEST_DIR ?
   fs.realpathSync(process.env.NODE_TEST_DIR) : path.resolve(__dirname, '..');
