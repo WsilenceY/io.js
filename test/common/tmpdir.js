17cf6761c190be57d5da5ad8793826dd5c1b6c76
Rich Trott <rtrott@gmail.com>
2018-01-03 19:58:58 -0800

    path is just a string now, but other vars and path module still exist

--- test/common/tmpdir.js
+++ test/common/tmpdir.js
@@ -62,7 +62,7 @@ let tmpdirName = '.tmp';
 //   tmpdirName += `.${process.env.TEST_THREAD_ID}`;
 // }
 // exports.path = path.join(testRoot, tmpdirName);
-exports.path = __dirname + '/../.tmp';
+exports.path = './../.tmp';
 
 // exports.refresh = () => {
 //   rimrafSync(exports.path);
