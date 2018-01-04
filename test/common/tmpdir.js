e0394b643ff0ed64960e89323512fdffef5b902d
Rich Trott <rtrott@gmail.com>
2018-01-03 20:15:04 -0800

    use path module

--- test/common/tmpdir.js
+++ test/common/tmpdir.js
@@ -61,8 +61,7 @@ let tmpdirName = '.tmp';
 // if (process.env.TEST_THREAD_ID) {
 //   tmpdirName += `.${process.env.TEST_THREAD_ID}`;
 // }
-// exports.path = path.join(testRoot, tmpdirName);
-exports.path = __dirname + '/../.tmp';
+exports.path = path.join(testRoot, tmpdirName);
 
 // exports.refresh = () => {
 //   rimrafSync(exports.path);
