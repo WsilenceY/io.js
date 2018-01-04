c1c13bc8cab584851c126a939e235a152c1a72d5
Rich Trott <rtrott@gmail.com>
2018-01-03 19:42:10 -0800

    add back some vars

--- test/common/tmpdir.js
+++ test/common/tmpdir.js
@@ -53,11 +53,11 @@ const path = require('path');
 // const testRoot = process.env.NODE_TEST_DIR ?
 //   fs.realpathSync(process.env.NODE_TEST_DIR) : path.resolve(__dirname, '..');
 // const testRoot = path.resolve(__dirname, '..');
-// const testRoot = __dirname + '/..';
+const testRoot = __dirname + '/..';
 
 // Using a `.` prefixed name, which is the convention for "hidden" on POSIX,
 // gets tools to ignore it by default or by simple rules, especially eslint.
-// let tmpdirName = '.tmp';
+let tmpdirName = '.tmp';
 // if (process.env.TEST_THREAD_ID) {
 //   tmpdirName += `.${process.env.TEST_THREAD_ID}`;
 // }
