eecbe9f361f92acdff96f1784ac072e4dc3edb0a
Rich Trott <rtrott@gmail.com>
2018-01-05 20:50:33 -0800

    import checkInvocations but do not invoke

--- test/async-hooks/test-zlib.zlib-binding.deflate.js
+++ test/async-hooks/test-zlib.zlib-binding.deflate.js
@@ -3,7 +3,7 @@
 const common = require('../common');
 const assert = require('assert');
 const initHooks = require('./init-hooks');
-// const { checkInvocations } = require('./hook-checks');
+const { checkInvocations } = require('./hook-checks');
 
 const hooks = initHooks();
 
